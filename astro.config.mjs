956e52650bb09128c15c1ec0f34db8ddb1f2b4b7
ECYDW <337174461@qq.com>
2026-06-13 22:59:53 +0800

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,247 @@
+import sitemap from "@astrojs/sitemap";
+import mdx from '@astrojs/mdx';
+import { unified } from '@astrojs/markdown-remark';
+import svelte, { vitePreprocess } from "@astrojs/svelte";
+import { pluginCollapsibleSections } from "@expressive-code/plugin-collapsible-sections";
+import { pluginLineNumbers } from "@expressive-code/plugin-line-numbers";
+import swup from "@swup/astro";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
+import expressiveCode from "astro-expressive-code";
+import icon from "astro-icon";
+import { oddmisc } from "oddmisc";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypeComponents from "rehype-components";
+import rehypeExternalLinks from "rehype-external-links";
+import rehypeKatex from "rehype-katex";
+import rehypeSlug from "rehype-slug";
+import remarkDirective from "remark-directive";
+import remarkMath from "remark-math";
+import remarkSectionize from "remark-sectionize";
+
+import { siteConfig } from "./src/config/index.ts";
+import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-copy-button.js";
+import { pluginLanguageBadge } from "./src/plugins/expressive-code/language-badge.ts";
+import { AdmonitionComponent } from "./src/plugins/rehype-component-admonition.mjs";
+import { GithubCardComponent } from "./src/plugins/rehype-component-github-card.mjs";
+import { rehypeImageWidth } from "./src/plugins/rehype-image-width.mjs";
+import { rehypeMermaid } from "./src/plugins/rehype-mermaid.mjs";
+import { rehypeWrapTable } from "./src/plugins/rehype-wrap-table.mjs";
+import { remarkContent } from "./src/plugins/remark-content.mjs";
+import { parseDirectiveNode } from "./src/plugins/remark-directive-rehype.js";
+import { remarkFixGithubAdmonitions } from "./src/plugins/remark-fix-github-admonitions.js";
+import { remarkMermaid } from "./src/plugins/remark-mermaid.js";
+
+// https://astro.build/config
+export default defineConfig({
+	site: siteConfig.siteURL,
+	trailingSlash: "always",
+
+	output: "static",
+
+	image: {
+		layout: "constrained",
+	},
+
+	server: {
+		port: 3000,
+	},
+
+	integrations: [
+		oddmisc({
+			umami: {
+				shareUrl: false,
+			},
+		}),
+		swup({
+			theme: false,
+			animationClass: "transition-swup-",
+			containers: ["main"],
+			smoothScrolling: false,
+			cache: true,
+			preload: false,
+			accessibility: true,
+			updateHead: process.env.NODE_ENV === "production",
+			updateBodyClass: false,
+			globalInstance: true,
+			resolveUrl: (url) => url,
+			animateHistoryBrowsing: false,
+			skipPopStateHandling: (event) => {
+				return (
+					event.state &&
+					event.state.url &&
+					event.state.url.includes("#")
+				);
+			},
+		}),
+		icon(),
+		expressiveCode({
+			themes: ["github-light", "github-dark"],
+			plugins: [
+				pluginCollapsibleSections(),
+				pluginLineNumbers(),
+				pluginLanguageBadge(),
+				pluginCustomCopyButton(),
+			],
+			defaultProps: {
+				wrap: true,
+				overridesByLang: {
+					shellsession: { showLineNumbers: false },
+					bash: { frame: "code" },
+					shell: { frame: "code" },
+					sh: { frame: "code" },
+					zsh: { frame: "code" },
+				},
+			},
+			styleOverrides: {
+				codeBackground: "var(--codeblock-bg)",
+				borderRadius: "0.75rem",
+				borderColor: "none",
+				codeFontSize: "0.875rem",
+				codeFontFamily:
+					"'JetBrains Mono Variable', SFMono-Regular, Menlo, Monaco, Consolas, 'Liberation Mono', 'Courier New', 'Microsoft JhengHei', '微軟正黑體', 'Microsoft YaHei', '微软雅黑', 'Noto Sans HK', 'Noto Sans TC', 'Noto Sans JP', 'Noto Sans SC', 'Noto Sans KR', ui-monospace, monospace",
+				codeLineHeight: "1.5rem",
+				frames: {
+					editorBackground: "var(--codeblock-bg)",
+					terminalBackground: "var(--codeblock-bg)",
+					terminalTitlebarBackground: "var(--codeblock-bg)",
+					editorTabBarBackground: "var(--codeblock-bg)",
+					editorActiveTabBackground: "none",
+					editorActiveTabIndicatorBottomColor: "var(--primary)",
+					editorActiveTabIndicatorTopColor: "none",
+					editorTabBarBorderBottomColor: "var(--codeblock-bg)",
+					terminalTitlebarBorderBottomColor: "none",
+				},
+				textMarkers: {
+					delHue: 0,
+					insHue: 180,
+					markHue: 250,
+				},
+			},
+			frames: {
+				showCopyToClipboardButton: false,
+			},
+		}),
+		svelte({
+			preprocess: vitePreprocess(),
+		}),
+		sitemap(),
+		mdx(),
+	],
+	markdown: {
+		processor: unified({
+			remarkPlugins: [
+				remarkMath,
+				remarkContent,
+				remarkFixGithubAdmonitions,
+				remarkDirective,
+				remarkSectionize,
+				parseDirectiveNode,
+				remarkMermaid,
+			],
+			rehypePlugins: [
+				rehypeKatex,
+				[
+					rehypeExternalLinks,
+					{
+						target: "_blank",
+						rel: ["nofollow", "noopener", "noreferrer"],
+					},
+				],
+				rehypeSlug,
+				rehypeWrapTable,
+				rehypeMermaid,
+				[
+					rehypeComponents,
+					{
+						components: {
+							github: GithubCardComponent,
+							note: (x, y) => AdmonitionComponent(x, y, "note"),
+							tip: (x, y) => AdmonitionComponent(x, y, "tip"),
+							important: (x, y) =>
+								AdmonitionComponent(x, y, "important"),
+							caution: (x, y) => AdmonitionComponent(x, y, "caution"),
+							warning: (x, y) => AdmonitionComponent(x, y, "warning"),
+						},
+					},
+				],
+				[
+					rehypeAutolinkHeadings,
+					{
+						behavior: "append",
+						properties: {
+							className: ["anchor"],
+						},
+						content: {
+							type: "element",
+							tagName: "span",
+							properties: {
+								className: ["anchor-icon"],
+								"data-pagefind-ignore": true,
+							},
+							children: [{ type: "text", value: "#" }],
+						},
+					},
+				],
+				rehypeImageWidth,
+			],
+		}),
+	},
+	vite: {
+		plugins: [tailwindcss()],
+		optimizeDeps: {
+			include: [
+				"@iconify/svelte",
+				"svelte",
+				"svelte/transition",
+				"svelte/easing",
+				"overlayscrollbars",
+				"@fancyapps/ui",
+				"marked",
+				"sanitize-html",
+				"qrcode",
+			],
+		},
+		server: {
+			warmup: {
+				clientFiles: [
+					"src/layouts/Layout.astro",
+					"src/pages/index.astro",
+					"src/components/widgets/music-player/MusicPlayer.svelte",
+					"src/components/organisms/navigation/Search.svelte",
+					"src/components/control/ThemeSwitch.svelte",
+					"src/components/features/settings/DisplaySettings.svelte",
+					"src/scripts/swup-manager.ts",
+				],
+			},
+		},
+		build: {
+			assetsInlineLimit: 4096,
+			cssCodeSplit: true,
+			cssMinify: "esbuild",
+			inlineStylesheets: "auto",
+			minify: "esbuild",
+			rollupOptions: {
+				onwarn(warning, warn) {
+					if (
+						warning.message.includes(
+							"is dynamically imported by",
+						) &&
+						warning.message.includes(
+							"but also statically imported by",
+						)
+					) {
+						return;
+					}
+					warn(warning);
+				},
+			},
+		},
+		esbuildOptions: {
+			drop:
+				process.env.NODE_ENV === "production"
+					? ["console", "debugger"]
+					: [],
+		},
+	},
+});
